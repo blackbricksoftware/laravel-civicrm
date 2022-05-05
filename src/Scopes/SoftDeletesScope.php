8fe9b6a85fcd173b8545798f7deaa68582a96998
David Hayes <david@blackbricksoftware.com>
2022-05-05 02:43:08 -0700

    fix class name

--- src/Scopes/SoftDeletesScope.php
+++ src/Scopes/SoftDeletesScope.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Scope;
  
-class VersioningScope implements Scope
+class SoftDeletesScope implements Scope
 {
     /**
      * Apply the scope to a given Eloquent query builder.
