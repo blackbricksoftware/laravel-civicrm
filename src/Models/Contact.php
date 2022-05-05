56ec471f248de596cbcc8e269916be02d8cdb8b5
David Hayes <david@blackbricksoftware.com>
2022-05-05 01:55:00 -0700

    woops forgot emails model

--- src/Models/Contact.php
+++ src/Models/Contact.php
@@ -18,6 +18,11 @@ class Contact extends Model
         return $this->hasMany(Address::class);
     }
 
+    public function emails(): HasMany
+    {
+        return $this->hasMany(Phone::class);
+    }
+
     public function phones(): HasMany
     {
         return $this->hasMany(Phone::class);
---
+++ src/Models/Email.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace BlackBrickSoftware\LaravelCiviCRM\Models;
+
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Email extends Model
+{
+    use HasFactory;
+
+    protected $table = 'civicrm_email';
+
+    public function contact(): BelongsTo
+    {
+        return $this->belongsTo(Contact::class);
+    }
+
+    public function locationType(): BelongsTo
+    {
+        return $this->belongsTo(LocationType::class);
+    }
+
+    public function scopePrimary(Builder $query): Builder
+    {
+        return $query->where('is_primary', 1);
+    }
+
+    public function scopeBilling(Builder $query): Builder
+    {
+        return $query->where('is_primary', 1);
+    }
+
+    public function scopeBulkMail(Builder $query): Builder
+    {
+        return $query->where('is_bulkmail', 1);
+    }
+}
