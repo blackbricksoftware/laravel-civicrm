14305ea4a88fcc9b439ddec7569996ac74792e3e
David Hayes <david@blackbricksoftware.com>
2022-05-08 03:41:29 -0700

    remove un used namespace

--- src/Models/Contact.php
+++ src/Models/Contact.php
@@ -5,7 +5,6 @@ namespace BlackBrickSoftware\LaravelCiviCRM\Models;
 use BlackBrickSoftware\LaravelCiviCRM\Scopes\SoftDeletesScope;
 use BlackBrickSoftware\LaravelCiviCRM\Traits\MultipleValueColumn;
 use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
